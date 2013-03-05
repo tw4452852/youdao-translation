14f48450b2484f43877daa456ece6451625e3e4e
Tw <tw19881113@gmail.com>
2013-03-05 15:52:32 +0800

    add main code

---
+++ tranlation.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/xml"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+var (
+	showHelp = flag.Bool("h", false, "show help")
+)
+
+func main() {
+	flag.Parse()
+
+	//must have at least one word
+	if flag.NArg() == 0 || *showHelp {
+		usage()
+		os.Exit(1)
+	}
+
+	translateWords(flag.Args())
+}
+
+func usage() {
+	fmt.Printf("%s word1 [word2...wordN]\n", os.Args[0])
+	flag.PrintDefaults()
+}
+
+type Results struct {
+	Entries []Entry `xml:"custom-translation>translation>content"`
+}
+
+type Entry struct {
+	Content string `xml:",chardata"`
+}
+
+func translateWords(words []string) {
+	const urlPrefix = "http://dict.youdao.com/fsearch?q="
+	for _, word := range words {
+		resp, err := http.Get(urlPrefix + word)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		resultXml, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		var result Results
+		if err := xml.Unmarshal(resultXml, &result); err != nil {
+			log.Println(err)
+			continue
+		}
+
+		resultS := ""
+		for _, ent := range result.Entries {
+			resultS += ent.Content
+		}
+		fmt.Println(resultS)
+	}
+}
